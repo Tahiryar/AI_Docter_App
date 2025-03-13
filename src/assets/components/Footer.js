45df0d931c8a4f12ab47a376eb760158bbb1cf52
Tahiryar <tahiryar.251k@gmail.com>
2025-03-13 21:49:21 +0500

    add footer

---
+++ src/assets/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2025 AI Doctor. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
