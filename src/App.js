c32e8109879b46fd5e9343483256b7fe030194a3
Tahiryar <tahiryar.251k@gmail.com>
2025-03-14 00:13:31 +0500

    Added CI workflow with development server start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './assets/components/Navbar';
+import Home from './assets/components/Home';
+import SymptomChecker from './assets/components/SymptomChecker';
+//import About from './assets/components/About'; // Import About component
+import Contact from './assets/components/Contact'; // Import Contact component
+import History from './assets/components/History'; // Import History component
+import Footer from './assets/components/Footer'; // Import Footer component
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/symptom-checker" element={<SymptomChecker />} />
+           {/*<Route path="/about" element={<About />} / {/* Route for About page */}
+          <Route path="/contact" element={<Contact />} /> {/* Route for Contact page */}
+          <Route path="/history" element={<History />} /> {/* Route for History page */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
